9c6848a2308541d2608fb853db2fe5a408f4fc9d
Issac Moreno <issaxavier909@gmail.com>
2019-12-20 10:59:12 -0800

    Not done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import Header from "./components/Header.js";
+import CharacterCard from './components/CharacterCard';
+import Welcome from './components/WelcomePage';
+import { Route } from 'react-router-dom';
+import axios from 'axios';
 
 
 export default function App() {
+  const [search, setSearch] = useState('');
+  const [searchResults, setResults] = useState ([]);
+  const [character, setCharacter] = useState('');
+
+  useEffect(() => {
+    axios
+    .get('https://rickandmortyapi.com/api/character/')
+    .then(res => {
+      console.log(res);  
+      res.data.results.map(char => {
+        console.log(char);
+        setCharacter(char)
+      })   
+      const results = res.data.results.filter(character =>
+      character.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setResults(results);
+    })
+    .catch(err => console.log(err));
+  }, [search])
+
+  const handleChange = event => {
+    setSearch(event.target.value)
+  };
+
   return (
     <main>
       <Header />
+      <Route exact path = '/'>
+        <Welcome />
+      </Route>
+      <Route exact path = '/characters'>
+        <div>
+          <ul>
+            {searchResults.map(character => (
+              <li key={character.id}>{character.name}</li>
+            ))}
+          </ul> 
+        </div>
+        <div>
+          {character.map (char => {
+            return <CharacterCard onChange = {handleChange} name = {char.name} search = {search} gender = {char.gender} status = {char.status} img = {char.image}  alt = {char.name} />
+            })}
+        </div>
+      </Route>
+        
     </main>
   );
 }
